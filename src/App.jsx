4bfc92748933936d18590e90baa7b2a0204739c5
coffmanjrp <coffmanjrp@gmail.com>
2021-08-08 23:24:24 -0700

    HTML and styles

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,8 @@ import './App.scss';
 function App() {
   return (
     <div className="App">
-      <h1>Hello World</h1>
+      <div className="toasts"></div>
+      <button className="btn">Show Notification</button>
     </div>
   );
 }
